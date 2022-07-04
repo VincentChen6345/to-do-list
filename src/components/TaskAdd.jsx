07227dd4d5ab38656a1db980bf6634d60b3eebfd
VincentChen6345 <vincent.chen6345@hotmail.com>
2022-07-04 18:12:00 +1000

    'input section update'

--- src/components/TaskAdd.jsx
+++ src/components/TaskAdd.jsx
@@ -4,14 +4,21 @@ const TaskAdd = () => {
   return (
     <div className="input__container">
       <form className="input__form">
-        <input type="text" className="input__field" placeholder="Add Task" />
-        <label>Priority</label>
-        <select className="input__dropdown">
-          <option value="high">high</option>
-          <option value="medium">medium</option>
-          <option value="low">low</option>
-        </select>
-        <button type="submit">Add new task</button>
+        <div className="container1">
+          <input type="text" className="input__field" placeholder="Add Task" />
+          <span className="input__priority">
+            <label className="priority__title">Priority</label>
+            <select className="input__dropdown">
+              <option value="high">high</option>
+
+              <option value="medium">medium</option>
+              <option value="low">low</option>
+            </select>
+          </span>
+        </div>
+        <button type="submit" className="input__submit-button">
+          Add new task
+        </button>
       </form>
     </div>
   );
