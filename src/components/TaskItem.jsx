554bcbeff7b983e5f75910b85f1ccc7b3ea78e18
VincentChen6345 <vincent.chen6345@hotmail.com>
2022-07-06 11:09:07 +1000

    'checkbox crossout function added'

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -2,25 +2,37 @@ import "./TaskAdd";
 import { ReactComponent as EditIcon } from "../assets/SVG/pencil.svg";
 import { ReactComponent as TrashIcon } from "../assets/SVG/bin.svg";
 import "./TaskItem.css";
+import React, { useState } from "react";
 
 const TaskItem = (props) => {
-  let className = "priority ";
-  if (props.priority === "high") className += "high";
-  if (props.priority === "medium") className += "medium";
-  if (props.priority === "low") className += "low";
+  let priorityClassName = "priority ";
+  if (props.priority === "high") priorityClassName += "high";
+  if (props.priority === "medium") priorityClassName += "medium";
+  if (props.priority === "low") priorityClassName += "low";
+
+  const [taskTitleClassName, setTaskTitleClassName] = useState("TaskTitle");
+  const checkboxChangeHandler = () => {
+    console.log("checkbox handler executed");
+
+    //when checked, add a 'checked' class to the class "TaskTitle" which crosses out the text
+    setTaskTitleClassName(
+      taskTitleClassName === "TaskTitle" ? "TaskTitle checked" : "TaskTitle"
+    );
+  };
 
   return (
     <div>
       <div className="TaskItem">
-        <span className="TaskTitle">{props.title}</span>
+        <span className={taskTitleClassName}>{props.title}</span>
         <span className="right-side">
-          <span className={className}>{props.priority}</span>
+          <span className={priorityClassName}>{props.priority}</span>
           <input
             className="checkbox"
             type="checkbox"
-            id="vehicle1"
-            name="vehicle1"
-            value="Bike"
+            // id="vehicle1"
+            // name="vehicle1"
+            // value="Bike"
+            onChange={checkboxChangeHandler}
           />
 
           <EditIcon className="EditIcon" />
